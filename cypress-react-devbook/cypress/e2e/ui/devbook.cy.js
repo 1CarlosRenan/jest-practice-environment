be03c689b934d6c0ad69d0387a9f9167b91fa5eb
1carlosrenan <carlosrenan96@gmail.com>
2023-02-26 15:25:01 -0300

    applying tdd to hero

--- cypress-react-devbook/cypress/e2e/ui/devbook.cy.js
+++ cypress-react-devbook/cypress/e2e/ui/devbook.cy.js
@@ -5,4 +5,15 @@ describe('Devbook app', () => {
     cy.visit('http://localhost:3000/') 
     cy.get('h2[data-test="heading"]').contains('Olá dev!')
   })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/') 
+    cy.get('div[data-test="book-list"]').should('exist')
+    cy.get('div.book-item').should((books) => {
+      expect(books).to.have.length(2);
+      const titles = [...books].map(book => book.querySelector('h2').innerHTML)
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+    })
+  })
 })
+
--- cypress-react-devbook/src/App.js
+++ cypress-react-devbook/src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">Olá dev!</Typography>
+      <div data-test="book-list">
+        <div className="book-item">
+          <Typography variant="h2" component="h2" data-test="heading">Refactoring</Typography>
+        </div>
+        <div className="book-item">
+          <Typography variant="h2" component="h2" data-test="heading">Domain-driven design</Typography>
+        </div>
+      </div>
     </>
   );
 }
