743843cf9961074c5ceb1dee71576843908bb619
1carlosrenan <carlosrenan96@gmail.com>
2023-02-26 15:52:28 -0300

    install json-server and axios

--- cypress-react-devbook/src/App.js
+++ cypress-react-devbook/src/App.js
@@ -1,14 +1,25 @@
 import { Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import BookList from "./components/BookList";
+import { api } from "./services/api";
 
 function App() {
-  const books = [
-    { name: "Refactoring" },
-    { name: "Domain-driven design" },
-  ]
+  const [books, setBooks] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await api.get('books')
+      setBooks(response.data)
+    }
+    fetchBooks()
+    setIsLoading(false)
+  }, [])
+
 
   return (
     <>
+      {isLoading && <Typography variant="h3" component="h3">Carregando...</Typography>}
       <Typography variant="h2" component="h2" data-test="heading">Olá dev!</Typography>
       <BookList books = { books} />
     </>
