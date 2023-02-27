b60c8cb9e1c58c5c96ff2ba71fd031097b5691d5
1CarlosRenan <carlosrenan96@gmail.com>
2023-02-27 20:23:51 -0300

    fix testing

--- cypress-react-devbook/cypress/e2e/ui/devbook.cy.js
+++ cypress-react-devbook/cypress/e2e/ui/devbook.cy.js
@@ -1,19 +1,48 @@
 /// <reference types="cypress" />
 
-describe('Devbook app', () => {
-  it('Visits the DevBook', () => {
-    cy.visit('http://localhost:3000/') 
-    cy.get('h2[data-test="heading"]').contains('Olá dev!')
-  })
+import { api } from "../../../src/services/api";
 
-  it('Shows a book list', () => {
-    cy.visit('http://localhost:3000/') 
-    cy.get('div[data-test="book-list"]').should('exist')
-    cy.get('div.book-item').should((books) => {
-      expect(books).to.have.length(2);
-      const titles = [...books].map(book => book.querySelector('h5').innerHTML)
-      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
-    })
-  })
-})
+describe("Devbook app", () => {
+  before(async () => {
+    return await api.delete("books?_cleanup=true").catch((err) => err);
+  });
 
+  beforeEach(() => {
+    const books = [
+      { name: "Building Microservices", id: 3 },
+      { name: "Domain-driven design", id: 2 },
+      { name: "Refactoring", id: 1 },
+    ];
+
+    return books.map((item) =>
+      api.post("books", item, {
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+  });
+
+  afterEach(() => {
+    return api.delete("books?_cleanup=true").catch((err) => err);
+  });
+
+  it("Visits the DevBook", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('h2[data-test="heading"]').contains("Olá dev!");
+  });
+
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(3);
+      const titles = [...books].map(
+        (book) => book.querySelector("h5").innerHTML
+      );
+      expect(titles).to.deep.equal([
+        "Refactoring",
+        "Domain-driven design",
+        "Building Microservices",
+      ]);
+    });
+  });
+});
