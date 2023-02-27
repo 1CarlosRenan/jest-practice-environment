bf8a198f3b7cb95046af07c6d517ca7041f6adcc
1CarlosRenan <carlosrenan96@gmail.com>
2023-02-27 20:08:23 -0300

    adding custom routes with json server

---
+++ cypress-react-devbook/server.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  if (req.method === "DELETE" && req.query["_cleanup"]) {
+    const db = router.db;
+    db.set("books", []).write();
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+});
+
+server.use(middlewares);
+
+server.use(router);
+
+server.listen(9000, () => {
+  console.log("JSON Server is runnig");
+});
